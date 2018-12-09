44d980bf632b59ad020d490ecec1c3ae244c85e0
TheRealChozoElder <nathanbeach@mail.weber.edu>
2018-12-09 05:05:37 -0700

    followed video and did my best on part one.

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,64 @@
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
-
+                    <h1>Please enter your information</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="firstName">First Name</label>
+                        <input
+                            type="text"
+                            id="firstName"
+                            class="form-control"
+                            v-model.lazy="userData.firstName">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastName">Last Name</label>
+                        <input
+                            type="text"
+                            id="lastName"
+                            class="form-control"
+                            v-model.lazy="userData.lastName">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                            type="text"
+                            id="email"
+                            class="form-control"
+                            v-model.lazy="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            class="form-control"
+                            v-model.lazy.trim="userData.password">
+                    </div>
+                    <div class="form-group">
+                        <label for="storeData">Store Data?</label>
+                        <br>
+                        <label for="yes">
+                            <input
+                                type="radio"
+                                id="yes"
+                                value="Yes"
+                                class="form-control"
+                                v-model="storeData"> Yes
+                        </label>
+                        &nbsp
+                        <label for="no">
+                            <input
+                                type="radio"
+                                id="no"
+                                value="No"
+                                class="form-control"
+                                v-model="storeData"> No
+                        </label>
+                        <br>
+                        <button
+                            class="btn btn-primary">Submit!</button>
+                    </div>
                     <!-- Part 2 5 Points -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
@@ -28,10 +85,10 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userData.firstName }} {{ userData.lastName }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -41,6 +98,17 @@
 
 <script>
     export default {
+        data() {
+            return{
+                userData: {
+                    email: '',
+                    password: '',
+                    firstName: '',
+                    lastName: ''
+                },
+                storeData: ''
+            }
+        }
     }
 </script>
 
